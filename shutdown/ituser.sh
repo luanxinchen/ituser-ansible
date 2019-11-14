aa9adb3cd1be7c4534c1aff1537632f47884d802
luanxinchen <1102026400@qq.com>
2019-11-14 13:20:26 +0800

    增加source支持

--- shutdown/ituser.sh
+++ shutdown/ituser.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-[ $UID != 0 ] && { echo "Error: You must be root to run this script"; exit 1; }
+[ $UID != 0 ] && { echo "Error: You must be root to run this script"; return; }
 name=ituser
 sshdir=/home/$name/.ssh
 pubkey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6g6QBvkZcMjHLT2m3pB8TafzEbvn4yAqIzzZx58lLyktF0mCt17u5Jj15TOkfI/76mYlhxV/W1J3xfQ6LRejab2vSrADHkVlhGcQ0JL98kkUEaMKxQQ6zrez7nYCNhpQMyyyKt7mzAChcovmfo2uLBLkOabQvRYZOs07vLIheltfo13GTRZ9IzOdOUhM83DEpqnEl5/1M3eS8Fmid7+HvMkmFsykxghSpHbha/376uty3b+ml71dazMLrAgdEZU6m9HZ+4c0xdAUCJLj1EBPAg1vCrv+neyMov686VcKpMYw5kBxZ+PsL1wLuWBsmntpQBkssEqa+YuEvtcRnD2hD root@localhost.localdomain"
 ipaddr=`ip addr | grep 'state UP' -A2 | grep '172.[12]6' | head -n 1 | awk '{print $2}' | cut -f1 -d '/'`
 hostname=`hostname -f`
-read -p "Please input the name of the server owner (use email prefix, such as zongyao.zhou) :" ownername
+read -p "Please input the name of the server owner(use email prefix, such as zongyao.zhou) :" ownername
 
 #user creat
 useradd $name
 if [ $? -eq 0 ];then
-   echo "user ${name} is created success!"
+   echo "user '${name}' is created success!"
    mkdir -p $sshdir
 else
-   echo "user ${name} is creating failed!"
-   exit 1
+   echo "user '${name}' is creating failed!"
+   echo "exit." 
+   #exit by return
+   return
 fi
 
 #pubkey import
@@ -63,10 +65,14 @@ fi
 rollback(){
    userdel $name
    rm -rf /home/$name
-   exit 1
+   echo "rollback & exit."
+   #exit by return
+   return
 }
 
 #status return
 curl http://172.16.2.33:8080/sinfo -X POST -d "ip=${ipaddr}&hn=${hostname}&on=${ownername}&sshst=${sshst}&sudost=${sudost}"
 
-exit
+#exit by return
+return
+
